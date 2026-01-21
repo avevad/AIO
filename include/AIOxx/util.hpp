9a43824ad4e5e726fd5f43ef7c9cfab53ac08e20
Vadim Averin <vadim@avevad.com>
2026-01-21 04:04:20 +0300

    Improve testsing and coverage (#30)
    * Preliminary structural improvements
    
    * Prepare for writing new tests
    
    * Add includes
    
    * Fixes
    
    * Add some tests
    
    * Add step to re-run failed tests
    
    * Improve pipeline
    
    * More improvements to pipeline
    
    * Final pipeline polishing
    
    * Fix pipeline
    
    * Fix LLVM version
    
    * Fix LLVM path
    
    * Downgrade to Clang 18
    
    * Change stdlib
    
    * Add libc++ dependency
    
    * Fix include
    
    * Bump Clang & LLVM to ver. 20

--- include/AIOxx/util.hpp
+++ include/AIOxx/util.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <exception>
 #include <expected>
 #include <iostream>
 #include <optional>
@@ -155,6 +156,7 @@ struct ExpectedResult {
   }
 
   static ExpectedResult make_err(std::exception_ptr err) {
+    AIOXX_ASSUME(err != nullptr);
     return {.expected = std::unexpected(std::move(err))};
   }
 
